3774d1a55ab897a61480fbddf1289d9352250a30
Pancosta <dz6.gustav@gmail.com>
2023-11-07 20:50:28 -0300

    correção migration de registros

--- CEEP/database/migrations/2023_09_21_210350_create_registros_table.php
+++ CEEP/database/migrations/2023_09_21_210350_create_registros_table.php
@@ -4,23 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRegistroTable extends Migration
+class CreateRegistrosTable extends Migration
 {
     public function up()
     {
-        Schema::create('registro', function (Blueprint $table) {
+        Schema::create('registros', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('rfid');
             $table->foreign('rfid')->references('id')->on('users')->onDelete('cascade');
             $table->dateTime('data_hora');
             $table->unsignedBigInteger('local_id');
-            $table->foreign('local_id')->references('id')->on('local')->onDelete('cascade');
+            $table->foreign('local_id')->references('id')->on('locals')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('registro');
+        Schema::dropIfExists('registros');
     }
 }

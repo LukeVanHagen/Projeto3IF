ab41b664fe2bf5f77ce444333aa0623ec54c08a5
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-11-21 18:42:50 -0300

    Atualizaçao guest.blade.php

--- CEEP/resources/views/layouts/guest.blade.php
+++ CEEP/resources/views/layouts/guest.blade.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <link href="{{ asset('css/logineregistro.css') }}" rel="stylesheet">
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="container">
-          <div class="login-box">
-                {{$slot}}
-          </div>   
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/logineregistro.css') }}">
+    <!-- Include any additional stylesheets or scripts here -->
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container">
+        <div class="login-box">
+            {{$slot}}
         </div>
-    </body>  
-    <!--<a href="/">
-                    <x-application-logo class="w-20 h-20 mt-20 fill-current text-gray-500" />
-                </a> -->
+    </div>
+</body>
 </html>

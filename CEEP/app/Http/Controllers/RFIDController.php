eddde608463273569aae202bcf1fa6c93ebdc37d
Pancosta <dz6.gustav@gmail.com>
2023-12-08 10:47:17 -0300

    entrada de alunos agora respeita os horarios pre-cadastrados

--- CEEP/app/Http/Controllers/RFIDController.php
+++ CEEP/app/Http/Controllers/RFIDController.php
@@ -6,40 +6,102 @@ use Illuminate\Http\Request;
 use App\Models\Aluno;
 use App\Models\User;
 use App\Models\Registro;
+use App\Models\Horario;
+use Carbon\Carbon;
 
 class RFIDController extends Controller
 {
     public function processarRFID(Request $request)
     {
         $rfid = $request->input('rfid');
-        $localId = $request->input('localId'); // Adicionando a obtenção do ID do local
+        $localId = $request->input('localId');
     
         info("RFID recebido: $rfid");
     
-        // Verificar se o RFID corresponde a um aluno
         $aluno = Aluno::where('rfid', $rfid)->first();
     
         info("Aluno encontrado: " . json_encode($aluno));
     
         if ($aluno) {
-            // Aluno autorizado
-            $this->registrarAcesso($rfid, $localId);
-            return response()->json(['message' => "Aluno {$aluno->nome} autorizado"]);
+            info("Verificando horário para aluno: {$aluno->nome}");
+            if ($this->verificarHorario($aluno)) {
+                $this->registrarAcesso($rfid, $localId);
+                return response()->json(['message' => "Aluno {$aluno->nome} autorizado"]);
+            } else {
+                return response()->json(['message' => "A turma do aluno não possui horário disponibilizado para este momento"], 403);
+            }
         }
     
-        // Se não for aluno, verifique se é um usuário
         $user = User::where('rfid', $rfid)->first();
     
         info("Usuário encontrado: " . json_encode($user));
     
         if ($user) {
-            // Usuário autorizado
             $this->registrarAcesso($rfid, $localId);
             return response()->json(['message' => "Usuário {$user->name} autorizado"]);
         }
     
-        // RFID não encontrado
         return response()->json(['message' => 'Usuário não autorizado'], 403);
+    }
+    
+    private function verificarHorario($aluno)
+    {
+        $diaAtual = $this->getDiaAtual();
+        $horaAtual = date('H:i');
+        info("Hora Atual: ".$horaAtual);
+
+        if ($horaAtual >= '06:00' && $horaAtual < '12:00') {
+            $periodoAtual = 'manha';
+        } elseif ($horaAtual >= '12:00' && $horaAtual < '18:00') {
+            $periodoAtual = 'tarde';
+        } else {
+            info("Acesso negado. Fora do horário permitido para a turma do aluno.");
+            return response()->json(['message' => 'Acesso negado. Fora do horário permitido para sua turma.'], 403);
+        }
+
+        // Obtenha a consulta SQL gerada
+        $consultaSQL = Horario::where([
+            'turma_id' => $aluno->turma_id,
+            'dia' => $diaAtual,
+            'periodo' => $periodoAtual,
+        ])->toSql();
+
+        // Exiba a consulta SQL (você pode usar info(), dd(), ou outra maneira de sua preferência)
+        info("Consulta SQL: " . $consultaSQL);
+
+        // Execute a consulta e obtenha o resultado
+        $horario = Horario::where([
+            'turma_id' => $aluno->turma_id,
+            'dia' => $diaAtual,
+            'periodo' => $periodoAtual,
+        ])->first();
+
+        return $horario !== null;
+    }
+
+    private function getDiaAtual()
+    {
+        // Obtém o nome do dia da semana em português
+        $diaAtual = Carbon::now()->locale('pt_BR')->dayName;
+    
+        // Adicione esta linha para exibir o valor de $diaAtual
+        info("Dia Atual: " . $diaAtual);
+    
+        // Mapeia o nome do dia para o formato esperado pela enumeração
+        $mapeamentoDias = [
+            'segunda-feira' => 'Segunda',
+            'terça-feira' => 'Terca',
+            'quarta-feira' => 'Quarta',
+            'quinta-feira' => 'Quinta',
+            'sexta-feira' => 'Sexta',
+            'sábado' => 'Sábado',
+            'domingo' => 'Domingo',
+        ];
+    
+        // Converte o nome do dia para o formato esperado pela enumeração
+        $diaFormatado = $mapeamentoDias[strtolower($diaAtual)];
+        info("Dia Formatado:".$diaFormatado);
+        return $diaFormatado;
     }
     
     private function registrarAcesso($rfid, $localId)

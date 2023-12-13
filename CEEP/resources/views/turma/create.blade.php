3dc64966341a6e1f7d7a56a0b52f5fb2889ec5c9
Emerson <emersonssantos321@gmail.com>
2023-12-13 16:09:40 -0300

    Alteração de permissão

--- CEEP/resources/views/turma/create.blade.php
+++ CEEP/resources/views/turma/create.blade.php
@@ -74,36 +74,36 @@
                 <!-- Adicionar para os outros dias e horários -->
 
                 <div class="text-end">
-                    <button type="submit" class="button-add">Salvar</button>
+                    <button  class="button-add">Salvar</button>
                 </div>
             </form>
         </div>
-        <br>
-        <div class="card-container">
+    </div>
+    <br>
+        <div class="card-conteiner">
             @foreach($turmas as $turma)
                 <div class="card-card">
-                    <div class="card-header text-center">{{ $turma->nome }}</div>
-                    <div class="card-body">
+                    <div class="card-heder text-center">{{ $turma->nome }}</div>
+                    <div class="card-bodi">
                         <!-- Adicionar descrição da turma se quiser-->
                     </div>
                     <!-- Botões de Editar e Excluir -->
-                    <div class="card-footer">
+                    <div class="card-foter">
                         <a href="{{ route('turma.edit', ['turma' => $turma->id]) }}">
-                            <img src="{{ asset('img/editar.png') }}" alt="Editar" class="footer-img">
+                            <img src="{{ asset('img/editar.png') }}" alt="Editar" class="foter-img">
                         </a>
 
                         <form action="{{ route('turma.destroy', ['turma' => $turma->id]) }}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit">
-                                <img src="{{ asset('img/excluir.png') }}" alt="Excluir" class="footer-img">
+                                <img src="{{ asset('img/excluir.png') }}" alt="Excluir" class="foter-img">
                             </button>
                         </form>
                     </div>
                 </div>
-                <br>
             @endforeach
         </div>
-    </div>
+    
 </div>
 @endsection

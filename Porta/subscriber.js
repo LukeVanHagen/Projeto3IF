ddaef3b5b4a0e23032782703bd6f00bc42b0fbd9
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-12-20 17:16:22 -0300

    Criação da parte lógica da porta de acordo com a mensagem RFID,se autorizada ou não autorizada subscriber.js

--- Porta/subscriber.js
+++ Porta/subscriber.js
@@ -1,6 +1,24 @@
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://localhost:1883'); // Altere para o endereço do seu servidor Mosquitto se necessário
 
+// Função para manipular a mensagem recebida
+function handleReceivedMessage(message) {
+const rfidStatus = JSON.parse(message);
+
+ // Adicione aqui a lógica para manipular o status recebido
+ if (rfidStatus.authorized) {
+  console.log('Porta autorizada - Atualizar interface com cor verde');
+  // Adicione lógica para atualizar a cor da porta na interface (usando jQuery)
+  $('#porta').removeClass('nao-autorizado').addClass('autorizado');
+} else {
+  console.log('Porta não autorizada - Atualizar interface com cor vermelha');
+  // Adicione lógica para atualizar a cor da porta na interface (usando jQuery)
+  $('#porta').removeClass('autorizado').addClass('nao-autorizado');
+}
+}
+
+
+
 client.on('connect', () => {
   console.log('Conectado ao servidor MQTT');
   client.subscribe('rfid-validation', (err) => {
@@ -14,5 +32,10 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
   console.log(`Mensagem recebida no tópico ${topic}: ${message.toString()}`);
-  // Adicione aqui o código para manipular a mensagem recebida
+  
+  // Chama a função para manipular a mensagem recebida
+  handleReceivedMessage(message.toString());
+  // Chama a função para atualizar o estado da porta com base na mensagem recebida
+  updatePortaState(JSON.parse(message.toString()));
 });
+
